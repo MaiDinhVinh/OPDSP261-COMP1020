5364f4eed8f037c8288be721a42e0309b30351a2
DucksaberVN <business@ducksabervn.com>
2026-03-02 15:01:26 +0700

    Create DoublyNode.java

---
+++ PS1/src/linkedlist/doublylinkedlist/DoublyNode.java
@@ -0,0 +1,11 @@
+package linkedlist.doublylinkedlist;
+
+public class DoublyNode {
+    public Integer value;
+    public DoublyNode previous;
+    public DoublyNode next;
+
+    public DoublyNode(Integer value){
+        this.value = value;
+    }
+}
