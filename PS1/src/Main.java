d28e1729521c14df008da9cfb9faaa7c126989a2
DucksaberVN <business@ducksabervn.com>
2026-03-05 19:24:29 +0700

    Update Main.java

--- PS1/src/Main.java
+++ PS1/src/Main.java
@@ -1,18 +1,16 @@
 import linkedlist.LinkedList;
 import linkedlist.Node;
+import stack.StackArray;
 
 public class Main {
     public static void main(String[] args) {
-        Node n1 = new Node(3);
-        Node n2 = new Node(5);
-        Node n3 = new Node(7);
-        LinkedList ll = new LinkedList();
-        ll.addToBack(n1);
-        ll.addToBack(n2);
-        ll.addToFront(n3);
-        ll.remove(Integer.valueOf(5));
-        Integer[] arr = ll.toArray(new Integer[0]);
-        for(int i: arr){
+        StackArray sa = new StackArray();
+        sa.push(1);
+        sa.push(2);
+        sa.push(3);
+//        System.out.println(sa.peek());
+        sa.pop();
+        for(int i: sa){
             System.out.println(i);
         }
     }
