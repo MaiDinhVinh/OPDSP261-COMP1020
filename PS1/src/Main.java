d0ce8802f9ed82caebeadb574eeb3a5110788eae
DucksaberVN <business@ducksabervn.com>
2026-03-16 18:44:26 +0700

    Update Main.java

--- PS1/src/Main.java
+++ PS1/src/Main.java
@@ -1,16 +1,24 @@
 import linkedlist.LinkedList;
 import linkedlist.Node;
+import queue.ArrayQueue;
+import queue.LinkedQueue;
+import queue.PriorityQueue;
 import stack.StackArray;
 
 public class Main {
     public static void main(String[] args) {
-        StackArray sa = new StackArray();
-        sa.push(1);
-        sa.push(2);
-        sa.push(3);
-//        System.out.println(sa.peek());
-        sa.pop();
-        for(int i: sa){
+        PriorityQueue pq = new PriorityQueue();
+        pq.enqueue(3);
+        pq.enqueue(1);
+        pq.enqueue(5);
+        pq.enqueue(2);
+        pq.enqueue(4);
+        pq.dequeue();
+        pq.dequeue();
+        pq.dequeue();
+        pq.dequeue();
+        pq.dequeue();
+        for(int i: pq){
             System.out.println(i);
         }
     }
