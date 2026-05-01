61dee4c2044e0b0a45352fc0f491ea7f337ebc82
DucksaberVN <business@ducksabervn.com>
2026-05-01 22:10:35 +0700

    Create Graphs.mm

---
+++ Graphs/Graphs.mm
@@ -0,0 +1,144 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="Graphs" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_1090958577" CREATED="1409300609620" MODIFIED="1777644118881" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<font BOLD="true"/>
+<hook NAME="MapStyle" background="#2e3440ff">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" auto_compact_layout="true" show_tags="UNDER_NODES" show_note_icons="true" associatedTemplateLocation="template:/dark_nord_template.mm" followedTemplateLocation="template:/dark_nord_template.mm" followedMapLastTime="1777644011212" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_671184412" ICON_SIZE="12 pt" FORMAT_AS_HYPERLINK="false" COLOR="#484747" BACKGROUND_COLOR="#eceff4" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt" NUMBERED="false" FORMAT="STANDARD_FORMAT" TEXT_ALIGN="DEFAULT" TEXT_WRITING_DIRECTION="LEFT_TO_RIGHT" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="1.9 px" BORDER_COLOR_LIKE_EDGE="true" BORDER_COLOR="#f0f0f0" BORDER_DASH_LIKE_EDGE="true" BORDER_DASH="SOLID" CHILD_NODES_LAYOUT="AUTO" VGAP_QUANTITY="2 pt" COMMON_HGAP_QUANTITY="14 pt">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#88c0d0" WIDTH="2" TRANSPARENCY="255" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_671184412" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Open Sans" SIZE="11" BOLD="false" UNDERLINED="false" STRIKETHROUGH="false" ITALIC="false"/>
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3" DASH="SOLID"/>
+<richcontent TYPE="DETAILS" CONTENT-TYPE="plain/auto"/>
+<richcontent TYPE="NOTE" CONTENT-TYPE="plain/auto"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details" BORDER_WIDTH="1.9 px">
+<edge STYLE="bezier" COLOR="#81a1c1" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ebcb8b">
+<icon BUILTIN="clock2"/>
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating" COLOR="#484747">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" COLOR="#e5e9f0" BACKGROUND_COLOR="#5e81ac" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#5e81ac"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_779275544" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#bf616a">
+<icon BUILTIN="yes"/>
+<arrowlink COLOR="#bf616a" TRANSPARENCY="255" DESTINATION="ID_779275544"/>
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.flower" COLOR="#ffffff" BACKGROUND_COLOR="#255aba" STYLE="oval" TEXT_ALIGN="CENTER" BORDER_WIDTH_LIKE_EDGE="false" BORDER_WIDTH="22 pt" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#f9d71c" BORDER_DASH_LIKE_EDGE="false" BORDER_DASH="CLOSE_DOTS" MAX_WIDTH="6 cm" MIN_WIDTH="3 cm"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#ffffff" BACKGROUND_COLOR="#484747" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font NAME="Open Sans" SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#eceff4" BACKGROUND_COLOR="#d08770" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="8 pt" SHAPE_VERTICAL_MARGIN="5 pt">
+<font NAME="Open Sans" SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#3b4252" BACKGROUND_COLOR="#ebcb8b">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#2e3440" BACKGROUND_COLOR="#a3be8c">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#2e3440" BACKGROUND_COLOR="#b48ead">
+<font SIZE="11"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" BACKGROUND_COLOR="#81a1c1">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" BACKGROUND_COLOR="#88c0d0">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" BACKGROUND_COLOR="#8fbcbb">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" BACKGROUND_COLOR="#d8dee9">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" BACKGROUND_COLOR="#e5e9f0">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" BACKGROUND_COLOR="#eceff4">
+<font SIZE="9"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<node TEXT="Why do we have Graphs ?" POSITION="bottom_or_right" ID="ID_646547205" CREATED="1777644201482" MODIFIED="1777644208866">
+<node TEXT="Trees can&apos;t represent data where each nodes has more than 1 parent" ID="ID_1175935584" CREATED="1777644209045" MODIFIED="1777644313450"/>
+</node>
+<node TEXT="Defitnition" POSITION="bottom_or_right" ID="ID_1276430247" CREATED="1777644467093" MODIFIED="1777644473455">
+<node TEXT="" ID="ID_761812940" CREATED="1777644674747" MODIFIED="1777644674747">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="A data structure that consists of a set of vertices (nodes) and a set of edges (connections). The edges represent the paths between edges" ID="ID_1135533910" CREATED="1777644473627" MODIFIED="1777644525959"/>
+<node TEXT="Both sets must be finite and either set can be empty (generally, if the edge set is empty then the vertice set is also empty)" ID="ID_34897771" CREATED="1777644526709" MODIFIED="1777644562100"/>
+<node TEXT="" ID="ID_1089534499" CREATED="1777644674737" MODIFIED="1777644674746">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="A graph can be represented like this&#xa;&#xa;E = set of edges&#xa;V = set of vertices" ID="ID_220131906" CREATED="1777644674753" MODIFIED="1777644717655">
+<node TEXT="\latex $$V = \{A, B, C, D, E\}$$" ID="ID_1481249561" CREATED="1777644718139" MODIFIED="1777644752673"/>
+<node TEXT="\latex E = \{\{A, B\}, \{A, D\}, \{C, E\}, \{D, E\}\}" ID="ID_497137853" CREATED="1777644737091" MODIFIED="1777644803325"/>
+</node>
+</node>
+</node>
+<node TEXT="Visual representation of graphs" POSITION="bottom_or_right" ID="ID_313616470" CREATED="1777644826552" MODIFIED="1777644835979">
+<node TEXT="Figure 10.1" ID="ID_76855143" CREATED="1777644836952" MODIFIED="1777644845535">
+<font BOLD="true" ITALIC="true"/>
+</node>
+<node TEXT="Figure 10.2" ID="ID_1393748725" CREATED="1777644962534" MODIFIED="1777644966589">
+<font BOLD="true" ITALIC="true"/>
+</node>
+</node>
+<node TEXT="Directed Graphs and Undirected Graphs" POSITION="bottom_or_right" ID="ID_1521638268" CREATED="1777645164696" MODIFIED="1777645173649">
+<node TEXT="Directed Graphs" ID="ID_840847984" CREATED="1777645239638" MODIFIED="1777645244778">
+<node TEXT="A type of graph with directional edges (arrows)" ID="ID_1588251852" CREATED="1777645245877" MODIFIED="1777645266830"/>
+</node>
+<node TEXT="Undirected Graphs" ID="ID_878303685" CREATED="1777645268542" MODIFIED="1777645272633">
+<node TEXT="A type of graph with non-directional edges (no arrows)" ID="ID_225023516" CREATED="1777645273137" MODIFIED="1777645287016"/>
+</node>
+<node TEXT="Notation" ID="ID_428496617" CREATED="1777645289204" MODIFIED="1777645290976">
+<node TEXT="" ID="ID_464363320" CREATED="1777645548190" MODIFIED="1777645548190">
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="Directed Graphs" ID="ID_1885273486" CREATED="1777645299724" MODIFIED="1777645303072">
+<node TEXT="\latex E  = \{(A, B), (B, A), (B, E), (D, A), (E, A), (E, C), (E, D)\}" ID="ID_1702325781" CREATED="1777645414356" MODIFIED="1777645486972"/>
+</node>
+<node TEXT="Undirected Graphs" ID="ID_1330108517" CREATED="1777645303600" MODIFIED="1777645307480">
+<node TEXT="\latex E  = \{\{A, B\}, \{B, A\}, \{B, E\}, \{D, A\}, \{E, A\}, \{E, C\}, E, D\}\}" ID="ID_1903151643" CREATED="1777645497491" MODIFIED="1777645544699"/>
+</node>
+<node TEXT="" ID="ID_1561441329" CREATED="1777645548185" MODIFIED="1777645548190">
+<hook NAME="SummaryNode"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Generally, a pair of (u, v) will represent directional edges while a set of {u, v} will represent non-directional edges" ID="ID_799187318" CREATED="1777645548193" MODIFIED="1777645579555"/>
+</node>
+</node>
+<node TEXT="Adjacent nodes" ID="ID_48916629" CREATED="1777645589686" MODIFIED="1777645595328">
+<node TEXT="If there is an edge from node &quot;u&quot; to node &quot;v&quot; =&gt; We call that node &quot;u&quot; is adjacent to node &quot;v&quot;" ID="ID_641318134" CREATED="1777645596219" MODIFIED="1777645642920">
+<node TEXT="For Directed Graphs" ID="ID_1991699707" CREATED="1777645831608" MODIFIED="1777645841710"/>
+</node>
+</node>
+</node>
+<node TEXT="NOTE: USE THE OTHER BOOK, NOT THE MAIN BOOK FROM THE COURSE (CHAPTER 10 !!!)" POSITION="top_or_left" ID="ID_197003669" CREATED="1777644619273" MODIFIED="1777644633079"/>
+</node>
+</map>
