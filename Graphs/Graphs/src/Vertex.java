f6b860cdd1b067a40b5e45443343935f11b73931
DucksaberVN <business@ducksabervn.com>
2026-05-03 12:44:02 +0700

    Create Vertex.java

---
+++ Graphs/Graphs/src/Vertex.java
@@ -0,0 +1,50 @@
+import java.util.ArrayList;
+
+public class Vertex<T extends Comparable<T>>{
+    private T value;
+    private ArrayList<Edge<T>> edges = new ArrayList<>();
+
+    //FOR BFS AND DFS ONLY !!! (idk, could be usable for other things)
+    private boolean isIdentified;
+    private boolean isProcessed;
+
+    public Vertex(T value){
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+
+    public ArrayList<Edge<T>> getEdges() {
+        return edges;
+    }
+
+    public void setValue(T value) {
+        this.value = value;
+    }
+
+    public void setEdges(ArrayList<Edge<T>> edges) {
+        this.edges = edges;
+    }
+
+    public void addEdge(Edge<T> edge){
+        this.edges.add(edge);
+    }
+
+    public boolean isProcessed() {
+        return isProcessed;
+    }
+
+    public boolean isIdentified() {
+        return isIdentified;
+    }
+
+    public void setProcessed(boolean processed) {
+        isProcessed = processed;
+    }
+
+    public void setIdentified(boolean identified) {
+        isIdentified = identified;
+    }
+}
